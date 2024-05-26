c008b67f656537c7ac4c230347d651b670accaba
Непряхин Алексей <nepyahinalex@gmail.com>
2024-05-26 15:30:20 +0300

    Добавлены ссылки и DI между слоями

---
+++ src/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Application;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddApplication();
+
+        return services;
+    }
+}
